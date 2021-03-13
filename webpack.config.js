31c098eea6ed944638e1571703993b6e8c49fe9b
Irena Jurasek <irenaskydive@gmail.com>
2021-03-13 18:11:59 +0100

    setup babel

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: '"babel-loader',
+                },
+            },
+        ],
+    },
+}
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: '"babel-loader',
+                },
+            },
+        ],
+    },
+}
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: '"babel-loader',
+                },
+            },
+        ],
+    },
+}
